8022c6e79c98b86d6b79b35cc1e0d6dd177ad990
Madison Newby <madison1999@hotmail.co.uk>
2023-05-23 10:20:53 +0100

    Fix timezones

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ function updateTime() {
   if (sydneyElement) {
     let sydneyDateElement = sydneyElement.querySelector(".date");
     let sydneyTimeElement = sydneyElement.querySelector(".time");
-    let sydneyTime = moment().tz("Pacific/Sydney");
+    let sydneyTime = moment().tz("Australia/Sydney");
 
     sydneyDateElement.innerHTML = sydneyTime.format("MMMM	Do YYYY");
     sydneyTimeElement.innerHTML = sydneyTime.format(
